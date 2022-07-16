fae775d183b60774889a69c06f868b76bdb0ff29
felipeapn <felipeapn@yahoo.com.br>
2022-07-16 12:28:49 -0300

    Project creation and settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RankingModule } from './ranking/ranking.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    RankingModule,
     AppRoutingModule
   ],
   providers: [],
